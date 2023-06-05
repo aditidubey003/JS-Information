187a6613f1826f4bcc092ef78f500d742d0db398
Aditi Dubey <aditidubey576@gmail.com>
2023-06-05 22:52:14 +0530

    completed the tasks

---
+++ Variables/script.js
@@ -0,0 +1,43 @@
+// TASK- 1    Working with variables
+
+// 1. Declare two variables: admin and name.
+
+let admin;
+let name;
+
+// 2. Assign the value "John" to name.
+name = "John"
+
+// 3. Copy the value from name to admin.
+admin = name;
+
+// 4. Show the value of admin using alert (must output “John”).
+
+alert(admin)
+
+
+// TASk - 2    Giving the right name
+
+
+// 1. Create a variable with the name of our planet. How would you name such a variable?
+
+const ourPlanet  = "Earth";
+
+// 2. Create a variable to store the name of a current visitor to a website. How would you name that variable?
+
+let currentVisitor;
+
+// TASK - 3 Uppercase const?
+
+// Examine the following code:
+
+const birthday = '18.04.1982';
+
+const age = someCode(birthday);
+
+// Here, We can use Uppercase for Birthdate  because it will not cahnge and remain same, but Our Age will keep changing 
+// as per time so we should not use uppercase for it.
+
+// We acn redeclare the birthday variable like -
+/
+const BIRTHDAY = '18.04.1982';
