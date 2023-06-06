ba1c4d9c4023989faa3805146a695ecf3ec67e8d
Aditi Dubey <aditidubey576@gmail.com>
2023-06-06 09:54:18 +0530

    completed few tasks

---
+++ Data Types/script.js
@@ -0,0 +1,13 @@
+
+// TASK - 1 String quotes
+
+// What is the output of the script?
+
+let name = "Ilya";
+
+
+alert( `hello ${1}` ); // hello 1
+
+alert( `hello ${"name"}` ); // hello name
+
+alert( `hello ${name}` ); // hello Ilya
---
+++ Interaction alert, prompt, confirm/script.js
@@ -0,0 +1,6 @@
+// Tasks
+// A simple page
+
+// Create a web-page that asks for a name and outputs it.
+
+alert(prompt("Whats's your name Sweetie?" ))
