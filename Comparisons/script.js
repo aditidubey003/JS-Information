e5ae028a134a615ae6144bcf3830e743c795141d
Aditi Dubey <aditidubey576@gmail.com>
2023-06-07 20:32:28 +0530

    Comparisons

---
+++ Comparisons/script.js
@@ -0,0 +1,18 @@
+// Tasks
+// Comparisons
+
+// What will be the result for these expressions?
+
+5 > 4 // True
+
+"apple" > "pineapple" // false
+
+"2" > "12" // true, as per dictionary comparison, the first character 2 is greater than other side first char 1.
+
+undefined == null // true
+
+undefined === null // false
+
+null == "\n0\n" // false
+
+null === +"\n0\n"// false
